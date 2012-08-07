e575a545682ac355e2bcf0ee96c41dfa9d434696
Sambit Behera <34sambit@gmail.com>
2012-08-06 22:05:01 -0700

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPuma::Application.config.secret_token = '7fa13096455034a1e7bf850961860887d5b0e6b099a9dc1c625a8cc5081f61de0106e93e267f622f92228670bbcbea599720414f64580d4d1ffb2f49e87db13f'
